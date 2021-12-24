08e5c363abc35cff5826bd9d0268ec5b7f291aea
Gabriel Silva Alves <gabrielsalves2k18@gmail.com>
2021-12-24 09:01:02 -0300

    i18n para classes e teste categoria

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  context 'Categoria não pode ser registrada' do
+    it 'e não cadastra pois os campos são obrigatórios' do
+      # Arrange
+      category = Category.create(name: '')
+
+      # Act
+      result = category.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+  end
 end
