8b97b55b9a77a7a1737680f119ba4e42aa199d10
Gabriel Silva Alves <gabrielsalves2k18@gmail.com>
2022-01-07 16:05:21 -0300

    Adiciona endpoint de listagem de galpões para API

--- spec/requests/warehouse_api_spec.rb
+++ spec/requests/warehouse_api_spec.rb
@@ -1,20 +1,38 @@
 require 'rails_helper'
 
 describe 'Warehouse API' do
-  it 'GET /warehouses' do
-    # Arrange
-    Warehouse.create!(name: 'Guarulhos', code: 'GRU', description: 'Teste cadastrar galpão', address: 'Rua Teste', city: 'Guarulhos',
-      state: 'SP', postal_code: '00000-000', total_area: '3000', useful_area: '2000')
-    Warehouse.create!(name: 'Porto Alegre', code: 'POA', description: 'Teste cadastrar galpão', address: 'Rua Teste', city: 'Porto Alegre',
-      state: 'RS', postal_code: '00000-000', total_area: '4000', useful_area: '3500')
-
-    # Act
-    get '/api/warehouses'
-
-    # Assert
-    expect(response).to have_http_status(200)
-    expect(response.content_type).to have_http_status(200)
-    expect(response.body).to include 'Maceió'
-    expect(response.body).to include 'Guarulhos'
+  context 'GET /api/v1/warehouse' do
+    it 'com sucesso' do
+      # Arrange
+      Warehouse.create!(name: 'Guarulhos', code: 'GRU', description: 'Teste cadastrar galpão', address: 'Rua Teste', city: 'Guarulhos',
+        state: 'SP', postal_code: '00000-000', total_area: '3000', useful_area: '2000')
+      Warehouse.create!(name: 'Porto Alegre', code: 'POA', description: 'Teste cadastrar galpão', address: 'Rua Teste', city: 'Porto Alegre',
+        state: 'RS', postal_code: '00000-000', total_area: '4000', useful_area: '3500')
+  
+      # Act
+      get '/api/v1/warehouses'
+  
+      # Assert
+      parsed_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to include('application/json')
+      expect(parsed_response[0]["name"]).to eq 'Guarulhos'
+      expect(parsed_response[1]["name"]).to eq 'Porto Alegre'
+      expect(response.body).not_to include 'Av Diadema'
+      expect(response.body).not_to include 'Av do Aeroporto'
+    end
+  
+    it 'resposta vazia' do
+      # Arrange
+  
+      # Act
+      get '/api/v1/warehouses'
+  
+      # Assert
+      parsed_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to include('application/json')
+      expect(parsed_response).to eq []
+    end
   end
 end
